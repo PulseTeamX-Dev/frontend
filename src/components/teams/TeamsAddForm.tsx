64f49eb1376791cb737f105180d7f4e94d8bd806
FernirTeam <90050382+OliaChuzhdiuk@users.noreply.github.com>
2026-06-22 17:24:53 +0300

    fix: update mobile layout and HR role logic

--- src/components/teams/TeamsAddForm.tsx
+++ src/components/teams/TeamsAddForm.tsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import { useAppDispatch } from "../../hooks/useReduxTypes";
-import { createTeam, fetchTeams } from "../../redux/teams/operation"; // <-- Додали імпорт fetchTeams для re-fetch
+import { createTeam, fetchTeams } from "../../redux/teams/operation";
 import { Input } from "../../shared/Input";
 import { Button } from "../../shared/Button";
 
