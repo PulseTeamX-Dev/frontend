a87a3736ffe5b81706cbedf1077322bb8d5f5db5
Holovko Maksym <golovkomaksim852@gmail.com>
2026-06-23 14:46:35 +0300

    bakup

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 export const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
-export const BASE_URL = "http://localhost:5000/api";
-// import.meta.env.VITE_API_URL || "https://pulseteamx-backend.onrender.com/api";
+export const BASE_URL =
+  import.meta.env.VITE_API_URL || "https://pulseteamx-backend.onrender.com/api";
 // http://localhost:5000/api
