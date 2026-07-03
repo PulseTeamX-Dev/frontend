5dc0279bdc7d1e09f30db00c887bd7c7ee8bc2ae
FernirTeam <90050382+OliaChuzhdiuk@users.noreply.github.com>
2026-07-03 20:46:05 +0300

    refactor: move supabase client to shared/api

--- src/components/profile/AvatarUploader.tsx
+++ src/components/profile/AvatarUploader.tsx
@@ -5,7 +5,7 @@ import { useAppDispatch, useAppSelector } from "../../hooks/useReduxTypes";
 
 import { selectProfileAvatar } from "../../redux/profile/selectors";
 import { updateProfile } from "../../redux/profile/operation";
-import { supabase } from "../../utils/supabase";
+import { supabase } from "@/shared/api/supabase";
 import { SUPABASE_URL } from "../../constants";
 
 const MAX_FILE_SIZE = 2 * 1024 * 1024;
